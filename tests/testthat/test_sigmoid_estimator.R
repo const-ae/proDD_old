3342843dfe30a4a334f08ba572aee11036cd896c
const-ae <artjom31415@googlemail.com>
2017-09-28 23:43:12 +0200

    Fix naming of conditions test_sigmoid_estimator

--- tests/testthat/test_sigmoid_estimator.R
+++ tests/testthat/test_sigmoid_estimator.R
@@ -32,9 +32,9 @@ test_that("sigmoid_estimation works", {
   data <- generate_zero_inflated_data_with_effect(N_genes=10, N_rep=3, perc_changed = 0, mu0=8.2, nu0=5, sigma0=0.4, location=8, scale=-0.3)
   
   X <- cbind(data$X, data$Y)
-  colnames(X) <- c(paste0("First_", 1:3), paste0("Second_", 1:3))
+  colnames(X) <- c(paste0("A_", 1:3), paste0("B_", 1:3))
   X <- X[rowSums(X) != 0, ]
-  data_description <-  data.frame(Condition=as.factor(c(rep("First", 3), rep("Second", 3))), 
+  data_description <-  data.frame(Condition=as.factor(c(rep("A", 3), rep("B", 3))), 
                                   Replicate=c(1:3, 1:3))
   data_description$Sample <- paste0(data_description$Condition, data_description$Replicate)
   
